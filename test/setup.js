69ba42670a4364da728dd44ad626c378546579a0
Dmitry Andriyanov <dima117a@yandex-team.ru>
2016-05-20 21:40:16 +0300

    positive tests

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,42 @@
 'use strict';
 
-var chai = require('chai');
+var chai = require('chai'),
+    validator = require('../lib/validator');;
 
 global.sinon = require('sinon');
 global.assert = chai.assert;
 
 sinon.assert.expose(chai.assert, {prefix: ''});
+
+global.validate = validator;
+
+global.checkValidObject = function(title, obj) {
+    var fullTitle = title + ': YES',
+        content = '(' + JSON.stringify(obj) + ')';
+
+    it(fullTitle, function() {
+        var errorCallback = sinon.spy();
+
+        validate(content, errorCallback);
+        assert.notCalled(errorCallback);
+    });
+}
+
+global.checkInvalidObject = function(title, obj, errorType, params) {
+    var fullTitle = title + ': NO',
+        content = '(' + JSON.stringify(obj) + ')';
+
+    it(fullTitle, function() {
+
+        var errorCallback = sinon.spy(),
+            arg1;
+
+        validate(content, errorCallback);
+
+        arg1 = errorCallback.getCall(0).args[1];
+
+        assert.calledOnce(errorCallback);
+        assert.strictEqual(arg1.keyword, errorType);
+        assert.deepEqual(arg1.params, params);
+    });
+}
--- test/validator.js
+++ test/validator.js
@@ -1,44 +1,97 @@
 'use strict';
 
-var validate = require('../lib/validator');
-
 describe('schema validator', function() {
 
-    describe('should pass id object is', function() {
+    describe('- root', function() {
 
-        it('empty array', function() {
-            checkValidObject([]);
+        checkValidObject('can be an array', [
+            { block: 'b-block-name'},
+            { block: 'b-block-name', elem: 'form' }
+        ]);
+
+        checkValidObject('can be an object', { block: 'b-page'});
+    });
+
+    describe('- entity', function() {
+        checkValidObject('can have plane fields', {
+            block: 'bBlock',
+            elem: 'elem',
+            mod: 'modName',
+            val: 'modValue',
+            tech: 'techName'
         });
 
-        it('not empty array', function() {
-            checkValidObject([
-                { block: 'b2' },
-                { block: 'b3' }
-            ]);
+        checkValidObject('can be empty', {});
+
+        checkValidObject('deps fields can be arrays', {
+            mustDeps: [{ block : 'b2' }],
+            shouldDeps: [{ block : 'b2' }],
+            noDeps: [{ block : 'b2' }]
         });
 
-        it('empty object', function() {
-            checkValidObject({});
+        checkValidObject('deps fields can be objects', {
+            mustDeps: { block : 'b2' },
+            shouldDeps: { block : 'b2' },
+            noDeps: { block : 'b2' }
+        });
+    });
+
+    describe('- dependency', function() {
+
+        checkValidObject('can have plane fields', {
+            mustDeps: {
+                tech: 'bemhtml',
+                block: 'b1',
+                elem: 'el1',
+                elems: []
+            }
         });
 
-        it('not empty object', function() {
-            checkValidObject({
-                block: 'b-block-name',
-                elem: 'elem-name',
-                mod: 'mod-name',
-                val: 'mod-value',
-                tech  : 'techName',
-                mustDeps   : [],
-                shouldDeps : [],
-                noDeps     : []
-            });
+        checkValidObject('can be empty', {});
+    });
+
+    describe('- mods', function() {
+
+        checkValidObject('must be an object', {});
+
+        checkValidObject('values must be strings', {
+            mustDeps: {
+                mods: { asd: 'qwfeg', wger: 'eet'}
+            }
+        });
+    });
+
+    describe('- elems', function() {
+
+        checkValidObject('can be an array', {
+           mustDeps: {
+               elems: []
+           }
+        });
+
+        checkInvalidObject( 'can not be an object', { mustDeps: { elems: { elem: 'test' } } }, 'type', { type: 'array' });
+    });
+
+    describe('- elems item', function() {
+
+        checkValidObject('can be a string', {
+           mustDeps: {
+               elems: ['el1', 'el2']
+           }
+        });
+
+        checkValidObject('can be an object', {
+           mustDeps: {
+               elems: [{ elem: 'el1' }, { elem: 'el2' }]
+           }
+        });
+
+        checkInvalidObject('must have elem name',  { mustDeps: { elems: [{ }] } }, 'required', { missingProperty: 'elem' });
+
+        checkValidObject('can have mods', {
+           mustDeps: {
+               elems: [{ elem: 'el1', mods: { asd: 'qwfeg', wger: 'eet'} }]
+           }
         });
     });
 });
-
-function checkValidObject(obj) {
-    var errorCallback = sinon.spy();
-
-    validate('(' + JSON.stringify(obj) + ')', errorCallback);
-    assert.notCalled(errorCallback);
-}
